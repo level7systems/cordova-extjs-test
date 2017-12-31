c6dbd46df3c5fb9c7acf6a125f3deeb3b3ba0642
chris <git@ssl7.net>
2017-12-31 09:03:38 +0100

    ExtJS app initialized

---
+++ app/model/Base.js
@@ -0,0 +1,7 @@
+Ext.define('CordovaExtJSTest.model.Base', {
+    extend: 'Ext.data.Model',
+
+    schema: {
+        namespace: 'CordovaExtJSTest.model'
+    }
+});
---
+++ app/model/Personnel.js
@@ -0,0 +1,7 @@
+Ext.define('CordovaExtJSTest.model.Personnel', {
+    extend: 'CordovaExtJSTest.model.Base',
+
+    fields: [
+        'name', 'email', 'phone'
+    ]
+});
